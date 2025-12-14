13c0073ad53e43e374ba8a952417d77f5868561e
S-ELANGO <selangobsccomputersciecne2006@gmail.com>
2025-12-14 18:40:01 +0530

    upload

---
+++ qrcode_generator/app.py
@@ -0,0 +1,11 @@
+import pyqrcode
+
+#get the url from users
+url=input("Enter the url:")
+
+#generate the qr code 
+qr=pyqrcode.create(url)
+qr.svg("qrcode.svg",scale=5)
+
+#show the qr code 
+qr.show('qrcode.svg')
