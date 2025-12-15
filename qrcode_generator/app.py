761b3c49ed38d520beaae72d398df275ace7d97d
S-ELANGO <selangobsccomputersciecne2006@gmail.com>
2025-12-15 12:30:22 +0530

    update

--- qrcode_generator/app.py
+++ qrcode_generator/app.py
@@ -5,7 +5,12 @@ url=input("Enter the url:")
 
 #generate the qr code 
 qr=pyqrcode.create(url)
-qr.svg("qrcode.svg",scale=5)
+qr.svg(
+     "qrcode.svg",
+     scale=6,
+     module_color=(255, 0, 0),      # Red QR
+     background=(255, 255, 255)     # White background
+)
 
 #show the qr code in terminal
 print(qr.terminal())
