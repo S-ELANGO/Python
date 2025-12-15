e00a8262f77a518c9f710815905f07ad58aa6277
S-ELANGO <selangobsccomputersciecne2006@gmail.com>
2025-12-15 20:58:12 +0530

    upload

---
+++ os_module/main.py
@@ -0,0 +1,15 @@
+import os
+#print the current working directory
+print(os.getcwd())
+
+#change the current working directory
+# os.chdir()
+
+#list the files in the current directory
+print(os.listdir())
+
+# create a new directory
+os.mkdir()
+# remove a directory
+os.rmdir()
+
