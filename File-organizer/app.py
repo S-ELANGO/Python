dbcdb88446a72728fae065963dbf8a30649f0bf7
S-ELANGO <selangobsccomputersciecne2006@gmail.com>
2026-01-10 17:41:14 +0530

    added the new file

---
+++ File-organizer/app.py
@@ -0,0 +1,29 @@
+import shutil
+from pathlib import Path
+
+target_dir = Path("/Users/elangosureshkumar/Desktop")
+
+cat = {
+    'Videos': ['mp4', 'mkv']
+}
+def organize_files(folder: Path):
+    for file in folder.iterdir():
+        if file.is_file():
+            moved = False
+
+            for category, extensions in cat.items():
+                if file.suffix.lower().lstrip('.') in extensions:
+                    category_dir = folder / category
+                    category_dir.mkdir(exist_ok=True)
+                    shutil.move(str(file), category_dir / file.name)
+                    moved = True
+                    break
+
+            if not moved:
+                other_dir = folder / 'Other'
+                other_dir.mkdir(exist_ok=True)
+                shutil.move(str(file), other_dir / file.name)
+
+if __name__ == '__main__':
+    organize_files(target_dir)
+    print("File organized successfully ✅")
